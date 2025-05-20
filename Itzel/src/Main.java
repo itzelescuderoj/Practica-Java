9483cef30bad5fc9f8b7df5aa1c64220c7aafdbc
itzelescuderoj <20243ds083@utez.edu.mx>
2025-05-20 09:50:02 -0600

    Agrega el codigo

---
+++ Itzel/src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        String num1 = JOptionPane.showInputDialog("Dame un numero");
+        String num2 = JOptionPane.showInputDialog("Dame un segundo numero");
+        String num3 = JOptionPane.showInputDialog("Dame un tercer numero");
+         int suma  = Integer.parseInt(num1) + Integer.parseInt(num2) + Integer.parseInt(num3);
+         System.out.println(suma /3);
+        
+        //System.out.println((Integer.parseInt(num1) + Integer.parseInt(num2) + Integer.parseInt(num3)) /3);
+
+        }
+    }
